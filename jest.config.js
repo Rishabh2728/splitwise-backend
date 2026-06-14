df06cd513da17b84cbd292de6dc6d2b9e710c485
Rishabh <rishabh@example.com>
2026-06-15 01:13:50 +0530

    build: exclude tests from TS build; replace jest.config.ts with jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/src/**/?(*.)+(spec|test).[jt]s?(x)']
+};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    preset: 'ts-jest',
+    testEnvironment: 'node'
+};
